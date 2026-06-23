1c9cc59967a64af86d7fcb471168b6e88d93ccd6
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2026-06-23 11:06:46 +0530

    🚀

---
+++ Examples/21/app.js
@@ -0,0 +1,6 @@
+/**
+ * Write a for loop to print numbers from 1 to 50.
+ */
+for (let i = 1; i <= 50; i++) {
+    console.log(`i = ${i}`);
+}
---
+++ Examples/22/app.js
@@ -0,0 +1,11 @@
+/**
+ * Use while loop to sum the numbers from 1 to 100 and print the result.
+ */
+let i = 1;
+let sum = 0;
+while (i <= 100) {
+    sum += i;
+    i++;
+}
+
+console.log(`The sum of numbers from 1 to 100 is: ${sum}`);
