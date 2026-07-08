888e0f30b789687b38a650ae1ff908482d58c497
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2026-07-08 18:34:44 +0530

    🚀

--- Examples/52/app.js
+++ Examples/52/app.js
@@ -1,78 +1,85 @@
-// Example #1
+// // Example #1
 
-function counter() {
-    let count = 0;
+// function counter() {
+//     let count = 0;
 
-    return function incrementCount() {
-        return count += 1;
-    }
+//     return function incrementCount() {
+//         return count += 1;
+//     }
+// }
+
+// let counterOne = counter();
+// let counterTwo = counter();
+
+// console.log(`counterOne ${counterOne()}`);
+// console.log(`counterOne ${counterOne()}`);
+// console.log(`counterOne ${counterOne()}`);
+// console.log(`counterOne ${counterOne()}`);
+// console.log(`counterOne ${counterOne()}`);
+
+// console.log(`counterTwo ${counterTwo()}`);
+// console.log(`counterTwo ${counterTwo()}`);
+// console.log(`counterTwo ${counterTwo()}`);
+
+// // Example #2
+
+// function Counter(value = 0) {
+//     this.value = value;
+
+//     this.increment = function() {
+//         this.value++;
+//     };
+
+//     this.decrement = function() {
+//         if (this.value > 0) {
+//             this.value--;
+//         }
+//     };
+
+//     this.reset = function() {
+//         this.value = 0;
+//     };
+// }
+
+// const counterOne = new Counter();
+// const counterTwo = new Counter();
+
+// console.log('Counter #1');
+
+// console.log(counterOne.value);
+
+// counterOne.increment();
+// counterOne.increment();
+// counterOne.increment();
+
+// console.log(counterOne.value);
+
+// counterOne.decrement();
+// counterOne.decrement();
+
+// console.log(counterOne.value);
+
+// counterOne.reset()
+
+// console.log(counterOne.value);
+
+
+// console.log('Counter #2');
+
+// console.log(counterTwo.value);
+
+// counterTwo.increment();
+// counterTwo.increment();
+
+// console.log(counterTwo.value);
+
+// counterTwo.reset();
+
+// console.log(counterTwo.value);
+
+const greet = function sayHello(name) {
+    console.log(sayHello); // the function itself is available since it is allocated memory before its execution
+    console.log(`Hello ${name} :)`);
 }
 
-let counterOne = counter();
-let counterTwo = counter();
-
-console.log(`counterOne ${counterOne()}`);
-console.log(`counterOne ${counterOne()}`);
-console.log(`counterOne ${counterOne()}`);
-console.log(`counterOne ${counterOne()}`);
-console.log(`counterOne ${counterOne()}`);
-
-console.log(`counterTwo ${counterTwo()}`);
-console.log(`counterTwo ${counterTwo()}`);
-console.log(`counterTwo ${counterTwo()}`);
-
-// Example #2
-
-function Counter(value = 0) {
-    this.value = value;
-
-    this.increment = function() {
-        this.value++;
-    };
-
-    this.decrement = function() {
-        if (this.value > 0) {
-            this.value--;
-        }
-    };
-
-    this.reset = function() {
-        this.value = 0;
-    };
-}
-
-const counterOne = new Counter();
-const counterTwo = new Counter();
-
-console.log('Counter #1');
-
-console.log(counterOne.value);
-
-counterOne.increment();
-counterOne.increment();
-counterOne.increment();
-
-console.log(counterOne.value);
-
-counterOne.decrement();
-counterOne.decrement();
-
-console.log(counterOne.value);
-
-counterOne.reset()
-
-console.log(counterOne.value);
-
-
-console.log('Counter #2');
-
-console.log(counterTwo.value);
-
-counterTwo.increment();
-counterTwo.increment();
-
-console.log(counterTwo.value);
-
-counterTwo.reset();
-
-console.log(counterTwo.value);
+greet('Mitesh');
